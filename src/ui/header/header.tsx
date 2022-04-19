f6c66b92995fc43bdac8f5e6d4a8469d0d61291c
Otto-Vector <50682440+Otto-Vector@users.noreply.github.com>
2022-04-19 23:00:10 +0300

    delete phone-module

--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './header.module.scss';
 import {Ecommerce} from './ecommerce/ecommerce';
-import {Phone} from './phone/phone';
 import {OnCallButton} from './on-call-button/on-call-button';
 
 type OwnProps = {}
@@ -12,13 +11,9 @@ export const Header: React.FC<OwnProps> = () => {
             <div className={styles.header__ecommerce}>
             <Ecommerce/>
             </div>
-            {/*<div className={styles.header__phone}>*/}
-            {/*    <Phone/>*/}
-            {/*</div>*/}
             <div className={styles.header__onCallButton}>
                 <OnCallButton/>
             </div>
-            {/*<p className={styles.header__description}>{'Ответим на любой вопрос'}</p>*/}
         </section>
     );
 }
